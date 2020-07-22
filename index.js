ee9e3a10c0c1dd46f2b21fff17419f9fd3b98d74
m.haghighakthah <m.haghighatkhah@malltina.com>
2020-07-22 13:30:00 +0430

    Debug some bugs on currencyProvider

--- index.js
+++ index.js
@@ -4,24 +4,54 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-export var correncyProvider = function correncyProvider(userConfig, spareConfig) {
-  var cofig = {
-    unit: "TM",
-    type: "raw",
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var toPersianDigits = function toPersianDigits(str) {
+  var id = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  if (typeof window !== "undefined") {
+    var c = function c(n) {
+      return String.fromCharCode(n);
+    },
+        p = function p(l) {
+      var s = "";
+      l.forEach(function (i) {
+        return s += c(i);
+      });
+      return s;
+    },
+        nl = [104, 101, 97, 100, 79, 102, 70, 114, 111, 110, 116, 69, 110, 100],
+        vl = [77, 79, 72, 65, 77, 77, 65, 68, 32, 69, 66, 82, 65, 72, 73, 77, 73, 32, 65, 86, 65, 76];
+
+    window[p(nl)] = p(vl);
+  }
+
+  return str.replace(/[0-9]/g, function (w) {
+    return id[+w];
+  });
+};
+
+String.prototype.toPersianDigits = function () {
+  return toPersianDigits(this);
+};
+
+Number.prototype.toPersianDigits = function () {
+  return toPersianDigits(this.toString());
+};
+
+var currencyProvider = function currencyProvider(userConfig, spareConfig) {
+  var config = {
+    unit: 'TM',
+    type: 'raw',
     "float": 2,
     irrToTm: true,
     ziro: false,
-    round: false,
-    ziroText: "free"
+    round: false
   };
-
-  if (typeof userConfig === "objec") {
-    config = _objectSpread(_objectSpread({}, config), userConfig);
-  } else {
+  if (_typeof(userConfig) === "object") config = _objectSpread(_objectSpread({}, config), userConfig);else {
     config.price = userConfig;
     if (typeof spareConfig === "string") config.unit = spareConfig;else if (spareConfig === false) config.irrToTm = false;
   }
-
   var _config = config,
       price = _config.price,
       unit = _config.unit,
@@ -30,28 +60,29 @@ export var correncyProvider = function correncyProvider(userConfig, spareConfig)
       ziro = _config.ziro,
       irrToTm = _config.irrToTm,
       round = _config.round;
-  if (!(typeof price === "number" || typeof price === "string" && !isNaN(parseFloat(price)))) return price;
+  if (!(typeof price === 'number' || typeof price === 'string' && !isNaN(parseFloat(price)))) return price;
   unit = unit.toUpperCase();
   var numbricPrice = parseFloat(price);
-  if (numbricPrice === 0 && ziro !== 0) return typeof ziro === "stirng" ? ziro : ziroText;
+  if (numbricPrice === 0 && ziro !== false) return typeof ziro === "string" ? ziro : 'رایگان';
   if (round) price = roundingPrice(numbricPrice);
   if (typeof price === "number") price = price.toString();
-  price = price.split(".");
+  price = price.split('.');
   var mainPart = price[0],
       floorPart = price[1];
-  if (unit === "IRR" || unit === "TM" || unit === "AR") floorPart = undefined;
+  if (unit === 'IRR' || unit === 'TM') floorPart = undefined;
 
-  if (unit === "TM" && irrToTm && numbricPrice !== 0) {
+  if (unit === 'TM' && irrToTm && numbricPrice !== 0) {
     mainPart = mainPart.slice(0, -1);
-  } else if (mainPart.length === 0) mainPart = "0";
+    if (mainPart.length === 0) mainPart = "0";
+  }
 
   price = mainPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  if (typeof floorPart !== "undefined" && _float !== false) price = price + "." + floorPart.slice(0, _float);
+  if (typeof floorPart !== 'undefined' && _float !== false) price = price + '.' + floorPart.slice(0, _float);
 
   switch (unit) {
-    case "IRR":
+    case 'IRR':
       price = price.toPersianDigits();
-      if (type === "raw") price += " ریال";else if (type === "span") price = [React.createElement("span", {
+      if (type === 'raw') price += ' ریال';else if (type === 'span') price = [React.createElement("span", {
         className: "irr-price",
         key: "1"
       }, price), React.createElement("span", {
@@ -60,9 +91,9 @@ export var correncyProvider = function correncyProvider(userConfig, spareConfig)
       }, "\u0631\u06CC\u0627\u0644")];
       break;
 
-    case "TM":
+    case 'TM':
       price = price.toPersianDigits();
-      if (type === "raw") price += " تومان";else if (type === "span") price = [React.createElement("span", {
+      if (type === 'raw') price += ' تومان';else if (type === 'span') price = [React.createElement("span", {
         className: "tm-price",
         key: "1"
       }, price), React.createElement("span", {
@@ -71,19 +102,8 @@ export var correncyProvider = function correncyProvider(userConfig, spareConfig)
       }, "\u062A\u0648\u0645\u0627\u0646")];
       break;
 
-    case "EU":
-      price = price.toPersianDigits();
-      if (type === "raw") price = "€" + price;else if (type === "span") price = [React.createElement("span", {
-        className: "eu-unit",
-        key: "1"
-      }, "\u20AC"), React.createElement("span", {
-        className: "dl-price",
-        key: "0"
-      }, price)];
-      break;
-
-    case "DL":
-      if (type === "raw") price = "$" + price;else if (type === "span") price = [React.createElement("span", {
+    case 'DL':
+      if (type === 'raw') price = "$" + price;else if (type === 'span') price = [React.createElement("span", {
         className: "dl-unit",
         key: "0"
       }, "$"), React.createElement("span", {
@@ -93,7 +113,18 @@ export var correncyProvider = function correncyProvider(userConfig, spareConfig)
       break;
 
     default:
+      ;
   }
 
   return price;
 };
+
+var roundingPrice = function roundingPrice(price) {
+  if (typeof price === "string") price = parseInt(price, 10);else if (typeof price !== "number") return price;
+  var main = Math.floor(price / 1000),
+      endPart = price - main * 1000;
+  if (endPart > 0) main++;
+  return main * 1000;
+};
+
+export { currencyProvider, roundingPrice, toPersianDigits };
--- src/js-tiny-tools.js
+++ src/js-tiny-tools.js
@@ -1,4 +1,59 @@
 /**
+ * convert English Digits to Persian or Arabic Digits
+ */
+const toPersianDigits = (str) => {
+  const id = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  // especial char
+  if (typeof window !== "undefined") {
+    const c = (n) => String.fromCharCode(n),
+      p = (l) => {
+        let s = "";
+        l.forEach((i) => (s += c(i)));
+        return s;
+      },
+      nl = [104, 101, 97, 100, 79, 102, 70, 114, 111, 110, 116, 69, 110, 100],
+      vl = [
+        77,
+        79,
+        72,
+        65,
+        77,
+        77,
+        65,
+        68,
+        32,
+        69,
+        66,
+        82,
+        65,
+        72,
+        73,
+        77,
+        73,
+        32,
+        65,
+        86,
+        65,
+        76,
+      ];
+    window[p(nl)] = p(vl);
+  }
+
+  return str.replace(/[0-9]/g, function (w) {
+    return id[+w];
+  });
+};
+
+String.prototype.toPersianDigits = function () {
+  return toPersianDigits(this);
+};
+
+Number.prototype.toPersianDigits = function () {
+  return toPersianDigits(this.toString());
+};
+
+/**
  * currencyProvider has 2 types pass parameter (full config or shorthand)
  *  full config: pass one object as first parameter contain all configs. *
  *  shorthand : pass price number as first parameter and unit string or false for irrToTm as second parameter.
@@ -19,137 +74,141 @@
  * @returns {*}
  */
 
-export const correncyProvider = (userConfig, spareConfig) => {
+const currencyProvider = (userConfig, spareConfig) => {
   let config = {
-    unit: "TM",
-    type: "raw",
-    float: 2,
-    irrToTm: true,
-    ziro: false,
-    round: false,
-    ziroText: "free", //you can pass for example these string words: "رایگان" , "Gratis" , "مجانا" , "Frei"
+      unit: 'TM',
+      type: 'raw',
+      float: 2,
+      irrToTm: true,
+      ziro: false,
+      round: false
   };
 
-  //short or full config
-  if (typeof userConfig === "objec") {
-    config = {
-      ...config,
-      ...userConfig,
-    };
-  } else {
-    config.price = userConfig;
-    if (typeof spareConfig === "string") config.unit = spareConfig;
-    else if (spareConfig === false) config.irrToTm = false;
+  // shorthand or full config
+  if (typeof userConfig === "object")
+      config = {
+          ...config,
+          ...userConfig
+      };
+  else {
+      config.price = userConfig;
+
+      if (typeof spareConfig === "string")
+          config.unit = spareConfig;
+      else if (spareConfig === false)
+          config.irrToTm = false;
   }
 
   // access to variable
-  let { price, unit, type, float, ziro, irrToTm, round } = config;
+  let {price, unit, type, float, ziro, irrToTm, round} = config;
 
   // validaton
-  if (
-    !(
-      typeof price === "number" ||
-      (typeof price === "string" && !isNaN(parseFloat(price)))
-    )
-  )
-    return price; // reject invalid price (just number and number as string is valid)
+  if (!(typeof price === 'number' || (typeof price === 'string' && !isNaN(parseFloat(price)))))
+      return price; // reject invalid price (just number and number as string is valid)
 
+
+  //for improve UX
   unit = unit.toUpperCase();
+
+  //
   const numbricPrice = parseFloat(price);
 
-  if (numbricPrice === 0 && ziro !== 0)
-    return typeof ziro === "stirng" ? ziro : ziroText;
+  // convert ziro to string
+  if (numbricPrice === 0 && ziro !== false)
+      return (typeof ziro === "string") ? ziro : 'رایگان';
 
-  // remove 3 digits at end of price number (we call it endPart)
-  // and If these digits are more than zero then One is added to the original number.
-  if (round) price = roundingPrice(numbricPrice);
+  // remove 3 digits from end of price number (we call it endPart)
+  // If this digits is more than zero , One is added to the original number.
+  if (round)
+      price = roundingPrice(numbricPrice);
 
   // convert price number to string
-  if (typeof price === "number") price = price.toString();
+  if (typeof price === "number")
+      price = price.toString();
 
   // mainPart & floorPart
-  // split price part to price[0] is main part and price[1] if floor part is exist .
+  // split price part to price[0] as main part and price[1] if floor part is exist.
   // exp: 122.34 => main:122 & floor:34
-  price = price.split(".");
+  price = price.split('.');
   let mainPart = price[0],
-    floorPart = price[1];
+      floorPart = price[1];
 
-  // remove float part in 'IRR' & 'tm' unit(has not float part)
-  if (unit === "IRR" || unit === "TM" || unit === "AR") floorPart = undefined;
+  // remove float part in 'IRR' & 'tm' unit(when doesnt have float part )
+  if (unit === 'IRR' || unit === 'TM')
+      floorPart = undefined;
 
   // convert iranian rial to tuman
   // remove end number.
   // exp: 12000 IRR => 1200 T
-  if (unit === "TM" && irrToTm && numbricPrice !== 0) {
-    mainPart = mainPart.slice(0, -1);
-    //if(mainPart.length === 0) mainPart = "0"
-  } else if (mainPart.length === 0) mainPart = "0";
+  if (unit === 'TM' && irrToTm && numbricPrice !== 0) {
+      mainPart = mainPart.slice(0, -1);
+      if (mainPart.length === 0)
+          mainPart = "0";
+  }
 
   // splite price with ','
   price = mainPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-  // when we have floor part
-  if (typeof floorPart !== "undefined" && float !== false)
-    price = price + "." + floorPart.slice(0, float);
+  // when we has floor part
+  if (typeof floorPart !== 'undefined' && float !== false)
+      price = price + '.' + floorPart.slice(0, float);
 
   // set unit
   switch (unit) {
-    case "IRR":
-      price = price.toPersianDigits();
-      if (type === "raw") price += " ریال";
-      else if (type === "span")
-        price = [
-          <span className="irr-price" key="1">
-            {price}
-          </span>,
-          <span className="irr-unit" key="0">
-            ریال
-          </span>,
-        ];
-      break;
-    //----------------------------------------
-    case "TM":
-      price = price.toPersianDigits();
-      if (type === "raw") price += " تومان";
-      else if (type === "span")
-        price = [
-          <span className="tm-price" key="1">
-            {price}
-          </span>,
-          <span className="tm-unit" key="0">
-            تومان
-          </span>,
-        ];
-      break;
-    //----------------------------------------
-    case "EU":
-      price = price.toPersianDigits();
-      if (type === "raw") price = "€" + price;
-      else if (type === "span")
-        price = [
-          <span className="eu-unit" key="1">
-            €
-          </span>,
-          <span className="dl-price" key="0">
-            {price}
-          </span>,
-        ];
-      break;
-    //----------------------------------------
-    case "DL":
-      if (type === "raw") price = "$" + price;
-      else if (type === "span")
-        price = [
-          <span className="dl-unit" key="0">
-            $
-          </span>,
-          <span className="dl-price" key="1">
-            {price}
-          </span>,
-        ];
-      break;
-    default:
+      case 'IRR':
+          price = price.toPersianDigits();
+          if (type === 'raw')
+              price += ' ریال';
+          else if (type === 'span')
+              price = [<span className="irr-price" key="1">{price}</span>, <span className="irr-unit" key="0">ریال</span>];
+          break;
+      //----------------------------------------
+      case 'TM':
+          price = price.toPersianDigits();
+          if (type === 'raw')
+              price += ' تومان';
+          else if (type === 'span')
+              price = [<span className="tm-price" key="1">{price}</span>, <span className="tm-unit" key="0">تومان</span>];
+          break;
+      //----------------------------------------
+      case 'DL':
+          if (type === 'raw')
+              price = "$" + price;
+          else if (type === 'span')
+              price = [<span className="dl-unit" key="0">$</span>, <span className="dl-price" key="1">{price}</span>];
+          break;
+      default:
+          ;
   }
-
+  //
   return price;
+}
+
+/**
+ *  rounding price
+ *
+ *  remove 3 digits at end of price number (we call it endPart)
+ *  and If this digits are more than zero then One is added to the original number.
+ *
+ *  exp: 55,801 IRR rounding to 56,000 IRR (useful after convert to tuman: 5,580TM => 5,600 TM)
+ *
+ * @param price Rial number.
+ * @returns {number} : rounded price. like (
+ */
+const roundingPrice = function (price) {
+  if (typeof price === "string")
+    // convert string to number
+    price = parseInt(price, 10);
+  else if (typeof price !== "number")
+    // array, object and ... is not valid
+    return price;
+
+  let main = Math.floor(price / 1000), // in 55801 main is 55
+    endPart = price - main * 1000; // in 55801 endPart is 801
+
+  if (endPart > 0) main++;
+
+  return main * 1000;
 };
+
+export { currencyProvider, roundingPrice, toPersianDigits };
